b9a137a81f0965fbcf32e67f3ac7f6b25bb48332
Fran242004 <salcedomendozafj@uvm.edu.ve>
2024-03-03 06:10:16 -0400

    Agregamos la funcionalidad de la bd de eventos

--- src/controllers/EventoController.js
+++ src/controllers/EventoController.js
@@ -1,82 +1,133 @@
+import { EventosModelo } from '../models-copy/EventosModelo.js';
+import { ProfesoresModelo } from '../models-copy/ProfesoresModelo.js';
+import { MateriasModelo } from '../models-copy/MateriasModelo.js';
+
 export class EventoController {
-    static async agregar(req, res) {
-        try {
-            const { tipo, descripcion, fecha, detalles, materiaId } = req.body;
-            const materia = Materia.buscarPorId(materiaId);
-            if (!materia) {
-                return res.status(404).json({ message: "Materia no encontrada, debe existir una materia" });
-            }
-            const evento = new Evento(tipo, descripcion, fecha, detalles, materia.id);
-            Evento.agregar(evento);
-            res.status(201).json({ message: "Evento agregado con éxito", evento });
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
+  static async agregar(req, res) {
+    try {
+      const { tipo, descripcion, fecha, detalles, materiaId } = req.body;
+      const materia = await MateriasModelo.findById(materiaId);
+      if (!materia) {
+        return res.status(404).json({ message: "Materia no encontrada, debe existir una materia" });
+      }
+      const evento = new EventosModelo({
+        tipo,
+        descripcion,
+        fecha,
+        detalles,
+        materiaId: materia._id
+      })
+      await evento.save();
+      res.status(201).json({ message: "Evento agregado con éxito", evento });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
+  }
 
-    static async listar(req, res) {
-        res.status(200).json(Evento.listar());
+  static async listar(req, res) {
+    try {
+      const eventos = await EventosModelo.find();
+      res.status(200).json(eventos);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
+  }
 
-    static async buscarPorId(req, res) {
-        const evento = Evento.buscarPorId(req.params.id);
-        if (evento) {
-            res.status(200).json(evento);
-        } else {
-            res.status(404).json({ message: "Evento no encontrado" });
-        }
-    }
-
-    static async actualizar(req, res) {
-        const eventoActualizado = Evento.actualizar(req.params.id, req.body);
-        if (eventoActualizado) {
-            res.status(200).json({ message: "Evento actualizado con éxito", evento: eventoActualizado });
-        } else {
-            res.status(404).json({ message: "Evento no encontrado" });
-        }
-    }
-
-    static async eliminar(req, res) {
-        const eventoEliminado = Evento.eliminar(req.params.id);
-        if (eventoEliminado) {
-            res.status(200).json({ message: "Evento eliminado con éxito"});
-        } else {
-            res.status(404).json({ message: "Evento no encontrado" });
-        }
-    }
-
-    static async filtrarEventosProximos(eventos) {
-        const hoy = new Date();
-        const dosSemanasMasTarde = new Date(hoy.getTime() + 14 * 24 * 60 * 60 * 1000);
-        return eventos.filter(evento => {
-          const fechaEvento = new Date(evento.fecha);
-          return fechaEvento >= hoy && fechaEvento <= dosSemanasMasTarde;
-        });
+  static async buscarPorId(req, res) {
+    try {
+      const evento = await EventosModelo.findById(req.params.id);
+      if (evento) {
+        res.status(200).json(evento);
+      } else {
+        res.status(404).json({ message: "Evento no encontrado" });
       }
 
-      static async listarEventosProximos(req, res) {
-        try {
-          const eventos = await Evento.listar(); 
-          const materias = await Materia.listar(); 
-          const profesores = await Profesor.listar(); 
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
 
-          const eventosProximos = await EventoController.filtrarEventosProximos(eventos);
-
-          const eventosProximosConMateriasYProfesores = eventosProximos.map(evento => {
-            const materia = materias.find(materia => materia.id === evento.materiaId);
-            const profesor = profesores.find(profesor => profesor.id === materia.profesorId);
-            if (!materia || !profesor) {
-              return res.status(404).json({ message: "No existe evento asociado con materia o profesor" });
-            }
-            return {
-              ...evento,
-              materia,
-              profesor
-            };
-          });
-          res.status(200).json(eventosProximosConMateriasYProfesores);
-        } catch (error) {
-          res.status(500).send({ message: "Error al obtener eventos próximos", error: error.message });
-        }
+  static async actualizar(req, res) {
+    try {
+      const eventoActualizado = await EventosModelo.findByIdAndUpdate(
+        req.params.id,
+        {
+          tipo: req.body.tipo,
+          descripcion: req.body.descripcion,
+          fecha: req.body.fecha,
+          detalles: req.body.detalles,
+        },
+        { new: true }
+      );
+      if (eventoActualizado) {
+        res.status(200).json({ message: "Evento actualizado con éxito", evento: eventoActualizado });
+      } else {
+        res.status(404).json({ message: "Evento no encontrado" });
       }
+
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  static async eliminar(req, res) {
+    try {
+      const eventoEliminado = await EventosModelo.findByIdAndDelete(req.params.id);
+      if (eventoEliminado) {
+        res.status(200).json({ message: "Evento eliminado con éxito" });
+      } else {
+        res.status(404).json({ message: "Evento no encontrado" });
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  static async filtrarEventosProximos(eventos) {
+    try {
+      const hoy = new Date();
+      const dosSemanasMasTarde = new Date(hoy.getTime() + 14 * 24 * 60 * 60 * 1000);
+      const eventosFiltrados = []
+
+      eventos.forEach(evento => {
+        const fechaEvento = new Date(evento.fecha);
+        if (fechaEvento >= hoy && fechaEvento <= dosSemanasMasTarde) {
+          eventosFiltrados.push(evento)
+        }
+      });
+
+      return eventosFiltrados
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  static async listarEventosProximos(req, res) {
+    try {
+      const eventos = await EventosModelo.find();
+
+      const eventosProximos = await EventoController.filtrarEventosProximos(eventos);
+      const eventosProximosConMateriasYProfesores = []
+
+      for (let i = 0; i < eventosProximos.length; i++) {
+        const materia = await MateriasModelo.findById(eventosProximos[i].materiaId);
+        const profesor = await ProfesoresModelo.findById(materia.profesorId);
+        if (!materia || !profesor) {
+          return res.status(404).json({ message: "No existe evento asociado con materia o profesor" });
+        }
+
+        const eventoProximo = {
+          evento: eventosProximos[i],
+          materia,
+          profesor
+        }
+
+        eventosProximosConMateriasYProfesores.push(eventoProximo)
+      }
+
+      res.status(200).json(eventosProximosConMateriasYProfesores);
+    } catch (error) {
+      res.status(500).send({ message: "Error al obtener eventos próximos", error: error.message });
+    }
+  }
 }
--- src/routes/RoutesEvento.js
+++ src/routes/RoutesEvento.js
@@ -3,11 +3,11 @@ import { EventoController } from '../controllers/EventoController.js';
 
 const router = express.Router();
 
-router.post('/eventos', EventoController.agregar);
-router.get('/eventos', EventoController.listar);
-router.get('/eventos/proximos', EventoController.listarEventosProximos);
-router.get('/eventos/:id', EventoController.buscarPorId);
-router.put('/eventos/:id', EventoController.actualizar);
-router.delete('/eventos/:id', EventoController.eliminar);
+router.post('/eventos', EventoController.agregar); // Funciona con la base de datos
+router.get('/eventos', EventoController.listar); // Funciona con la base de datos
+router.get('/eventos/proximos', EventoController.listarEventosProximos); // Funciona con la base de datos
+router.get('/eventos/:id', EventoController.buscarPorId); // Funciona con la base de datos
+router.put('/eventos/:id', EventoController.actualizar); // Funciona con la base de datos
+router.delete('/eventos/:id', EventoController.eliminar); // Funciona con la base de datos
 
 export default router;
